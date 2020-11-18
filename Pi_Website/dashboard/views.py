5f16616faabca0e56917138d93d9bd85adcacea3
Nick Speth <nicholass410@gmail.com>
2020-11-18 13:50:09 -0500

    Added username variable to the dashboard

--- Pi_Website/dashboard/views.py
+++ Pi_Website/dashboard/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+userInfo = [
+    {
+        'username': 'Test User',
+        'password': 'Test'
+    }
+]
 # Create your views here.
 
 def login(request):
     return HttpResponse('<h1>Login</h1>')
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    user = {
+        'userInfo': userInfo
+    }
+    return render(request, 'dashboard/dashboard.html', user)
 
