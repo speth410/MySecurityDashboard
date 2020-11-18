39758854b9dd87ab470252615d220e3f44cf1f0b
Nick Speth <nicholass410@gmail.com>
2020-11-18 14:18:47 -0500

    Login GUI added

--- Pi_Website/dashboard/urls.py
+++ Pi_Website/dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'dashboard'
 urlpatterns = [
     path('', views.dashboard, name='dashboard-home'),
-    path('login/', views.login, name='dashboard-login'),
+    path('login/', views.user_login, name='user_login'),
+    
 ]
--- Pi_Website/dashboard/views.py
+++ Pi_Website/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 userInfo = [
     {
@@ -10,11 +11,37 @@ userInfo = [
 # Create your views here.
 
 def login(request):
-    return HttpResponse('<h1>Login</h1>')
+    return render(request, 'dashboard/login.html')
 
 def dashboard(request):
     user = {
         'userInfo': userInfo
     }
+
     return render(request, 'dashboard/dashboard.html', user)
+
+
+def user_login(request):
+    return render(request, 'dashboard/login.html')
+    '''
+    if request.method == 'POST':
+        # Process the request if posted data are available
+        username = request.POST['username']
+        password = request.POST['password']
+        # Check username and password combination if correct
+        user = authenticate(username=username, password=password)
+
+        
+        if user is not None:
+            # Save session as cookie to login the user
+            login(request, user)
+            # Success, now let's login the user.
+            return render(request, 'templates/dashboard/account.html')
+        else:
+            # Incorrect credentials, let's throw an error to the screen.
+            return render(request, 'ecommerce/user/login.html', {'error_message': 'Incorrect username and / or password.'})
+    else:
+        # No post data availabe, let's just show the page to the user.
+        return render(request, 'ecommerce/user/login.html')
+        '''
 
